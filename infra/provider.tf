b320d7349a7ce4dc0d023b577846401315e3bb4a
tushar9861 <tusardas7788@gmail.com>
2025-12-06 00:35:54 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,11 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>5.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
