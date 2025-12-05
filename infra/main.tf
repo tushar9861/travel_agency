cde35cb7f71de776f24958ec3235511c504482e8
tushar9861 <tusardas7788@gmail.com>
2025-12-06 02:50:19 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,35 +1,74 @@
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
+# -----------------------------
+# VPC
+# -----------------------------
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-# Use DEFAULT VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Use default VPC subnet (public)
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  tags = {
+    Name = "travel-agency-vpc"
   }
 }
 
-# Pick first subnet
-locals {
-  public_subnet_id = data.aws_subnets.default.ids[0]
+# -----------------------------
+# Subnet
+# -----------------------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "travel-agency-public-subnet"
+  }
 }
 
+# -----------------------------
+# Internet Gateway
+# -----------------------------
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "travel-agency-igw"
+  }
+}
+
+# -----------------------------
+# Route Table
+# -----------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "travel-agency-public-rt"
+  }
+}
+
+# -----------------------------
+# Route Table Association
+# -----------------------------
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# -----------------------------
 # Security Group
+# -----------------------------
 resource "aws_security_group" "app_sg" {
   name        = "travel-agency-app-sg"
   description = "Allow HTTP & SSH"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "HTTP"
+    description = "HTTP access"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -37,7 +76,7 @@ resource "aws_security_group" "app_sg" {
   }
 
   ingress {
-    description = "SSH"
+    description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -56,21 +95,19 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# -----------------------------
 # EC2 Instance
+# -----------------------------
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  subnet_id = local.public_subnet_id
-
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.app_sg.id]
+  key_name                    = var.key_name
+  associate_public_ip_address = true
 
   tags = {
     Name = "travel-agency-app"
   }
 }
 
-output "app_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
