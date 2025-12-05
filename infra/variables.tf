e573c8ba0edbca14cf7754fa7a0632463ada9add
tushar9861 <tusardas7788@gmail.com>
2025-12-06 01:20:48 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,21 +1,30 @@
+// infra/variables.tf
+
+// Region where you want to create the app EC2
 variable "aws_region" {
-  type = string
+  type    = string
+  default = "us-east-1"
 }
 
+// EC2 instance type for the app server
 variable "instance_type" {
   type    = string
-  default = "t2.micro"
+  default = "t2.small"
 }
 
+// AMI of your Jenkins EC2 (same region us-east-1)
 variable "ami_id" {
-  type = string
+  type    = string
+  default = "ami-0ecb62995f68bb549"
 }
 
+// EC2 Key Pair name (from AWS console, e.g., key2)
 variable "key_name" {
-  type = string
+  type    = string
+  default = "key2"
 }
 
-# Added for Jenkins → Terraform authentication
+// These will be passed from Jenkins credentials
 variable "aws_access_key" {
   type = string
 }
