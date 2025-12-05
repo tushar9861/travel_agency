f413fe2c2ab3fda9f3861443f6ef4c664e32dae9
tushar9861 <tusardas7788@gmail.com>
2025-12-06 00:36:12 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,41 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-03deb8c961063af8c" # Ubuntu AMI
-  instance_type = "t2.micro"
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
 
-  subnet_id              = var.public_subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
-    Name = "travel-agency-app-server"
+    Name = "travel-agency-app"
   }
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "travel-agency-sg"
+  description = "Allow inbound HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.app_server.public_ip
 }
