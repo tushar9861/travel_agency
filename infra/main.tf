16dfc63b69a3514ea57e4c0faf41aa965af82bf3
tushar9861 <tusardas7788@gmail.com>
2025-12-06 01:21:23 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,29 +1,22 @@
-resource "aws_instance" "app_server" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
-
-  tags = {
-    Name = "travel-agency-app"
-  }
-}
+// infra/main.tf
 
 resource "aws_security_group" "app_sg" {
-  name        = "travel-agency-sg"
-  description = "Allow inbound HTTP and SSH"
+  name        = "travel-agency-app-sg"
+  description = "Allow HTTP and SSH for app EC2"
+  vpc_id      = null # default VPC in us-east-1
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -34,8 +27,24 @@ resource "aws_security_group" "app_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "travel-agency-app-sg"
+  }
 }
 
-output "public_ip" {
-  value = aws_instance.app_server.public_ip
+resource "aws_instance" "app_server" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = {
+    Name = "travel-agency-app"
+  }
+}
+
+output "app_public_ip" {
+  description = "Public IP of the app EC2"
+  value       = aws_instance.app_server.public_ip
 }
