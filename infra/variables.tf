f30b16f445f6075c8820145bd553af524929a5cb
tushar9861 <tusardas7788@gmail.com>
2025-12-06 00:35:31 +0530

    Add AWS credentials and EC2 instance variables
    Added AWS credentials and EC2 instance variables.

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,29 @@
-variable "public_subnet_id" {
-  description = "Public subnet ID where EC2 will be launched"
+# AWS Credentials (passed securely from Jenkins)
+variable "aws_access_key" {
+  description = "AWS access key from Jenkins"
   type        = string
 }
 
-variable "vpc_security_group_ids" {
-  description = "Security groups to attach to EC2"
-  type        = list(string)
+variable "aws_secret_key" {
+  description = "AWS secret key from Jenkins"
+  type        = string
+}
+
+# EC2 instance variables
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to launch EC2 instance"
+  type        = string
+  default     = "ami-06b21ccaeff8cd686" # Ubuntu 22 LTS (N. Virginia)
 }
 
 variable "key_name" {
-  description = "Name of the SSH key pair"
+  description = "SSH key pair name"
   type        = string
+  default     = "my-key"
 }
