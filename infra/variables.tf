535bd1bd6b8149e28da91d742e527d2ff5762a55
tushar9861 <tusardas7788@gmail.com>
2025-12-06 02:49:52 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,31 @@
 variable "aws_region" {
-  type = string
+  type        = string
+  default     = "ap-south-1"
+  description = "AWS region"
 }
 
-variable "instance_type" {
-  type    = string
-  default = "t2.small"
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key from Jenkins"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key from Jenkins"
 }
 
 variable "ami_id" {
-  type = string
+  type        = string
+  description = "AMI ID for EC2 instance"
 }
 
 variable "key_name" {
-  type = string
+  type        = string
+  description = "Existing EC2 Key Pair name"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "instance_type" {
+  type        = string
+  default     = "t2.small"
+  description = "Instance type for EC2"
+}
