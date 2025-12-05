f206eca15698b48d59c57fc6b9447c9e0e4ba757
tushar9861 <tusardas7788@gmail.com>
2025-12-06 00:42:53 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,29 +1,16 @@
-# AWS Credentials (passed securely from Jenkins)
-variable "aws_access_key" {
-  description = "AWS access key from Jenkins"
-  type        = string
+variable "aws_region" {
+  type = string
 }
 
-variable "aws_secret_key" {
-  description = "AWS secret key from Jenkins"
-  type        = string
-}
-
-# EC2 instance variables
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+  type    = string
+  default = "t2.micro"
 }
 
 variable "ami_id" {
-  description = "AMI ID to launch EC2 instance"
-  type        = string
-  default     = "ami-06b21ccaeff8cd686" # Ubuntu 22 LTS (N. Virginia)
+  type    = string
 }
 
 variable "key_name" {
-  description = "SSH key pair name"
-  type        = string
-  default     = "my-key"
+  type = string
 }
