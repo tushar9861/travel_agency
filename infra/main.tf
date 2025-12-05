d6cbfada5a12399bc87f933a5d09f2f6f216eeb5
tushar9861 <tusardas7788@gmail.com>
2025-12-05 19:23:06 +0530

    Modify EC2 instance settings in main.tf
    Updated EC2 instance configuration for application deployment, including AMI and instance name changes.

--- infra/main.tf
+++ infra/main.tf
@@ -1,50 +1,12 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
-
-
-############################################
-# EC2 Instance for Application Deployment
-############################################
-
 resource "aws_instance" "app_server" {
-  ami           = "ami-03deb8c961063af8c" # Ubuntu 22.04 LTS (same AMI as Jenkins)
+  ami           = "ami-03deb8c961063af8c" # Ubuntu AMI
   instance_type = "t2.micro"
 
   subnet_id              = var.public_subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = [var.vpc_security_group_ids]
   key_name               = var.key_name
 
   tags = {
-    Name = "CICD-App-Server"
+    Name = "travel-agency-app-server"
   }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    apt update -y
-    apt install -y docker.io
-    systemctl start docker
-    systemctl enable docker
-  EOF
 }
-
-############################################
-# Outputs
-############################################
-
-output "app_server_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
-
