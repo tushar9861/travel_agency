2e8bb57b7540b267e8e3a71acec09368d101b0bc
tushar9861 <tusardas7788@gmail.com>
2025-12-06 11:23:54 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,69 +1,114 @@
+########################################
+# Data sources
+########################################
+
+# Get available AZs so we can pick one
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Get latest Amazon Linux 2 AMI (x86_64, EBS gp2)
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+########################################
+# Networking (VPC, Subnet, IGW, Route Table)
+########################################
+
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "travel-agency-vpc"
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
   }
 }
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "travel-agency-public-subnet"
+    Name = "${var.project_name}-public-subnet"
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "travel-agency-igw"
-  }
-}
-
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "travel-agency-public-rt"
+    Name = "${var.project_name}-public-rt"
   }
 }
 
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.public.id
 }
 
+########################################
+# Security Group
+########################################
+
 resource "aws_security_group" "app_sg" {
-  name        = "travel-agency-app-sg"
-  description = "Allow HTTP & SSH"
+  name        = "${var.project_name}-sg"
+  description = "Allow HTTP and SSH"
   vpc_id      = aws_vpc.main.id
 
+  # SSH
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
+    description = "SSH access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -71,18 +116,22 @@ resource "aws_security_group" "app_sg" {
   }
 
   tags = {
-    Name = "travel-agency-app-sg"
+    Name = "${var.project_name}-sg"
   }
 }
 
+########################################
+# EC2 Instance
+########################################
+
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
-  key_name      = var.key_name
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  key_name               = var.key_name
 
   tags = {
-    Name = "travel-agency-app"
+    Name = "${var.project_name}-app-server"
   }
 }
