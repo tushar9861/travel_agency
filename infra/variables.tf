0b1aefcfaab2c0f03555dca6e9e47094a3aa1b77
tushar9861 <tusardas7788@gmail.com>
2025-12-06 16:06:00 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,31 +1,35 @@
-variable "aws_region" {
-  default = "ap-south-1"
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key for provider authentication"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key for provider authentication"
+  sensitive   = true
+}
 
 variable "ami_id" {
-  type = string
-  default = "ami-0ecb62995f68bb549" # your confirmed AMI
+  type        = string
+  description = "AMI ID for the EC2 instance"
 }
 
 variable "key_name" {
-  type = string
-  default = "key2"
+  type        = string
+  description = "Existing EC2 key pair name"
 }
 
 variable "instance_type" {
-  type = string
-  default = "t2.small"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "subnet_id" {
   type        = string
-  description = "Subnet ID where EC2 should be launched"
+  description = "Subnet ID where the EC2 instance will be placed"
 }
 
 variable "security_group_id" {
   type        = string
-  description = "Security group ID for EC2 instance"
+  description = "Security group ID for the EC2 instance"
 }
