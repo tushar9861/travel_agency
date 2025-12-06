98023d2ef90d59e41d530c4b738a9b87d2f38c46
tushar9861 <tusardas7788@gmail.com>
2025-12-06 14:35:10 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,12 +1,21 @@
-variable "ami_id" {
-  type = string
+variable "aws_region" {
+  default = "ap-south-1"
 }
 
-variable "instance_type" {
-  type    = string
-  default = "t2.micro"
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "ami_id" {
+  type = string
+  default = "ami-0ecb62995f68bb549" # your confirmed AMI
 }
 
 variable "key_name" {
   type = string
+  default = "key2"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
 }
