b8c4be3f442edb9dad19b5c12e7dba4c51e1b7a9
tushar9861 <tusardas7788@gmail.com>
2025-12-06 14:18:29 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.5.0"
-}
-
 provider "aws" {
   region = var.aws_region
-  # Credentials are taken from:
-  # - Environment variables (AWS_ACCESS_KEY_ID / AWS_SECRET_ACCESS_KEY)
-  # - Or IAM role attached to the instance
 }
