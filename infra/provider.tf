81576ce9bac999f64ca49f270f56059e455917c4
tushar9861 <tusardas7788@gmail.com>
2025-12-09 14:11:40 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,10 +5,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  # For CI use, passing access/secret via variables (set via Jenkins) is OK.
+  # Do NOT check real credentials into git.
 }
