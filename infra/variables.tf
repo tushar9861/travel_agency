1a41be7d6b68723242a540a8220b6a8770f46e1a
tushar9861 <tusardas7788@gmail.com>
2025-12-09 14:23:39 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,30 +4,39 @@ variable "aws_region" {
 }
 
 variable "aws_access_key" {
-  type = string
+  type        = string
+  description = "AWS access key (passed securely from Jenkins credentials)"
+  sensitive   = true
 }
 
 variable "aws_secret_key" {
-  type = string
+  type        = string
+  description = "AWS secret key (passed securely from Jenkins credentials)"
+  sensitive   = true
 }
 
 variable "ami_id" {
   type    = string
+  default = "ami-0ecb62995f68bb549" # change if needed
 }
 
 variable "instance_type" {
   type    = string
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "key_name" {
   type    = string
+  default = "key1" # existing key pair name in your account
 }
 
-variable "subnet_id" {
-  type = string
+variable "jenkins_ip_cidr" {
+  type    = string
+  description = "CIDR for Jenkins server SSH access (eg. 13.60.238.195/32)"
 }
 
-variable "security_group_id" {
-  type = string
+variable "public_ssh_from" {
+  type    = string
+  default = "0.0.0.0/0"
+  description = "If you want SSH from anywhere (not recommended). Default wide-open. Prefer setting jenkins_ip_cidr."
 }
