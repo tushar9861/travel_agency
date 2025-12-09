7db5c01be03305148cb145ce2a6a9c4a19a6a8b1
tushar9861 <tusardas7788@gmail.com>
2025-12-09 14:27:03 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,11 @@
+output "app_instance_id" {
+  value = aws_instance.app_server.id
+}
+
 output "app_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value = aws_instance.app_server.public_ip
+}
+
+output "app_public_dns" {
+  value = aws_instance.app_server.public_dns
 }
