efbe35a22297c9e1e054e7ae9dfe10f476ce520f
tushar9861 <tusardas7788@gmail.com>
2025-12-11 18:41:03 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,1 @@
-output "app_instance_id" {
-  value = aws_instance.app_server.id
-}
 
-output "app_public_ip" {
-  value = aws_instance.app_server.public_ip
-}
-
-output "app_public_dns" {
-  value = aws_instance.app_server.public_dns
-}
