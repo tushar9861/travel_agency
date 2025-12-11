88344e7b94444a6331773fd5b60e3106f4f6b188
tushar9861 <tusardas7788@gmail.com>
2025-12-11 18:41:13 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,17 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
 
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  # For CI use, passing access/secret via variables (set via Jenkins) is OK.
-  # Do NOT check real credentials into git.
-}
