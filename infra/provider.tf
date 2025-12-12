e43fdb2dd058563858ab5b8acc3d4b586c4cb8da
tushar9861 <tusardas7788@gmail.com>
2025-12-12 11:35:58 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::634526447081:role/terraform_role"
+    session_name = "terraform"
+  }
+}
 
